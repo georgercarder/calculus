98c90ceb63443205f89646b188cb0f44d8a20fc3
George Carder <georgercarder@gmail.com>
2018-01-18 14:00:08 -0800

    derive expession is looking good. need to suppress 1* term

--- calculus/polynomial.py
+++ calculus/polynomial.py
@@ -21,29 +21,50 @@ class expression:
 	def derive(self, wrt):
 		self.__wrt=wrt
 		self.simplify()
-		__terms=self.string.split('+')
+		__terms=self.getString().split('+')
+
+		self.func('hey')
 		for i in range(len(__terms)):
 			if re.findall(self.__wrt,__terms[i])==[]:
 				pass
 			else:
-				__terms[i]=deriveterm(__terms[i],wrt) #need to define deriveterm
+				__terms[i]=self.deriveterm(__terms[i],wrt) 
 		__derived=""
 		for i in range(len(__terms)):
 			__derived=__derived+__terms[i]+'+'	
 		__derived=__derived.rstrip('+')
 		return __derived
 
-	def deriveterm(__term, wrt):
-		if len(re.findall( wrt, __term))==0:
-			return 0
+	def func(self,x):
+		print(x)
+	
+	def deriveterm(self,__term,__wrt):
+		__term=__term
+		__wrt=__wrt
+		if len(re.findall(__wrt, __term))==0:
+			return "" 
 		else:
 			if len(re.findall('^\d*',__term))==0:
 				__c=1
 			else:
 				__c=re.findall('^\d*',__term)
 
-			__wrtex=wrt+'\^*\d*'
-### here grep ^ to see if exponent is 1:
+			__wrtex=__wrt+'\^*\d*'
+			__wrtexp=re.findall(__wrtex,__term)
+			if len(re.findall('\^',__wrtexp[0]))==0:
+				__exp=1
+			else:
+				__exp=re.findall('\d+$',__wrtexp[0])
+			print(__c, __exp)
+			__c=int(__c[0])*int(__exp[0])
+			print(__c,__wrt,__exp)
+			if int(__exp[0])!=2:
+				self.__derivedterm=str(__c)+str(__wrt)+"^"+str(int(__exp[0])-1)
+			else:
+				self.__derivedterm=str(__c)+str(__wrt)
+	
+			return self.__derivedterm
+###
 
 	#simplifies '2x^2+5x^3y^5+2y+2x^3y^5' to '2x^2+7x^3y^5+2y'
 
