67f12acc180d092223ba6c53e0669d26c2515235
George Carder <georgercarder@gmail.com>
2018-01-22 21:31:10 -0800

    named simplifier function in cal*/de*

--- calculus/decomposition.py
+++ calculus/decomposition.py
@@ -14,24 +14,24 @@ import re
 
 a='2(x^3+(2y(z+4u+3z)^2-3(x+86v^2-(22x+y+5x)))-72)^3'
 
+def simplifier(a):
+	n=a.count('(')
 
-n=a.count('(')
-
-while n>0:
-X=re.findall('[0-9]*\([a-z0-9A-Z\+-^]+\)\^*[0-9]*',a)
-XX=X
+	while n>0:
+	X=re.findall('[0-9]*\([a-z0-9A-Z\+-^]+\)\^*[0-9]*',a)
+	XX=X
 	# simplify each of these finite sums
-for i in range(len(X)):
-	S=re.findall('\([a-z0-9A-Z\+-^]+\)',X[i])
-	finsum=finitesum(S[0].strip('(').strip(')'))
-	finsum.simplify()
-	XX[i]=X[i].replace(S[0].strip('(').strip(')'),finsum.getString())
+	for i in range(len(X)):
+		S=re.findall('\([a-z0-9A-Z\+-^]+\)',X[i])
+		finsum=finitesum(S[0].strip('(').strip(')'))
+		finsum.simplify()
+		XX[i]=X[i].replace(S[0].strip('(').strip(')'),finsum.getString())
 
 	# now multipy
-	P=finiteproduct(XX[i])
-	P.simplify()
-	XX[i]=P.getString()
+		P=finiteproduct(XX[i])
+		P.simplify()
+		XX[i]=P.getString()
 
-	a.replace(X[i],XX[i])
-	n-=1
+		a.replace(X[i],XX[i])
+		n-=1
 
